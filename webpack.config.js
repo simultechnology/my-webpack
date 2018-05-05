62e628d72e312e6b2143bdcae3856bed329e5686
t_ishikawa <t_ishikawa@zenrin-datacom.net>
2018-05-05 12:09:37 +0900

    style-loader,css-loaderの追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
-const path = require('path'); 
+const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: `./src/index.js`,
-    output: {
-      path: `${__dirname}/dist`,
-      filename: 'main.js'
-    },
-    devServer: {
-      contentBase: path.join(__dirname, "dist"),
-      compress: true,
-      port: 39000,
-      open: true
-    },
+  mode: 'development',
+  entry: `./src/index.js`,
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          'style-loader',
+          {loader: 'css-loader', options: {url: false}},
+        ],
+      },
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 39000,
+    open: true
+  },
 };
   
